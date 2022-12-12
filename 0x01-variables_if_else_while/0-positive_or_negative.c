75e80055727f9f5b1e39987ac854902cd1a79cde
teemsha <teemsha003@gmail.com>
2022-12-12 15:55:08 -0800

    My 1st C - Variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,13 +4,10 @@
 
 #include <stdio.h>
 
-
-
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- *
- */
+ ** main - Entry point
+ ** Return: Always 0 (Success)
+ **/
 
 int main(void)
 
@@ -22,10 +19,10 @@ int main(void)
 
 	if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n < 0)							
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	else										printf("%d is positive\n", n);
-
+	else
+		printf("%d is positive\n", n);
 	return (0);
 
 }
