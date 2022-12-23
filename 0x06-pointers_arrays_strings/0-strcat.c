f4dca614de69bb276d00ce30746d66646791a5dd
teemsha <teemsha003@gmail.com>
2022-12-23 08:37:57 -0800

    My first C - More pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,31 @@
 #include "main.h"
+
 /**
- * _strcat - concatenates two strings,
+ * _strcat - concatenated two strings
+ * @dest: string to append to
+ * @src: string to to
+ * @src: sting to add
  *
- * @dest: destination.
- * @src: source.
- * Return: the pointer to dest.
- *
+ * Return: a pointer to the resulting string
  */
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+	int i, j;
 
-	while (*(dest + count) != '\0')
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
 	{
-		count++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
-	while (count2 >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
+
+	dest[i] = '\0';
+
 	return (dest);
 }
