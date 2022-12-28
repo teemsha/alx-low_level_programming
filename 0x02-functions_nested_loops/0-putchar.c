c041a965319e0314a918e8ac4b2034e14ce7a37d
teemsha <teemsha003@gmail.com>
2022-12-27 22:23:49 -0800

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,25 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - Prints Holberton as a message.
+ * main - Programe entry point
+ *
+ * Description: This is a program to print putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
 	return (0);
 }
